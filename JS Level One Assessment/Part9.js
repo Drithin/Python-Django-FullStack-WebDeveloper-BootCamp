30a5e1dc963c3f59dc42d19e13166b7b3f56f59a
Drithin <drithn@gmail.com>
2017-06-17 13:49:13 -0700

    feat: Java Script Level One Asseessment, posted in forums for code review, yet to get a reply from instructor

---
+++ JS Level One Assessment/Part9.js
@@ -0,0 +1,14 @@
+var pQ1 = prompt("Enter you First Name: ");
+var pQ2 = prompt("Enter you Last Name: ");
+var pQ3 = prompt("Enter you Age: ");
+var pQ4 = prompt("Enter you Height: ");
+var pQ5 = prompt("Enter you Pet Name: ");
+
+if (pQ1[0] == pQ2[0] &&
+  pQ4 == 170 &&
+  pQ5[(pQ5.length)-1]== "y"){
+
+while(pQ3 < 30 && pQ3 > 20){
+  console.log("Welcome to Spy World");
+}
+  }
