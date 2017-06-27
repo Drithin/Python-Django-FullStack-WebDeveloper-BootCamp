104545601a7fc67745cdcbc7af250d54624e5649
Drithin <drithn@gmail.com>
2017-06-26 20:39:36 -0700

    feat: JavaScript Level Two Assessment

---
+++ Advanced JS Assessment/Part6_Objects_Exercise.js
@@ -0,0 +1,64 @@
+// Part 6 - Objects Exercise
+
+////////////////////
+// PROBLEM 1 //////
+//////////////////
+
+// Given the object:
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31
+  namelength: function(){
+    console.log("Length of Employee Name = "+ this.name.length);
+  }
+
+}
+
+// Add a method called nameLength that prints out the
+// length of the employees name to the console.
+
+
+///////////////////
+// PROBLEM 2 /////
+/////////////////
+
+// Given the object:
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31
+
+  report : function(){
+    alert("Name : "+this.name+", "+"Job : "+this.job+", "+"Age : "+this.age);
+  }
+}
+
+// Write program that will create an Alert in the browser of each of the
+// object's values for the key value pairs. For example, it should alert:
+
+// Name is John Smith, Job is Programmer, Age is 31.
+
+
+
+///////////////////
+// PROBLEM 3 /////
+/////////////////
+
+// Given the object:
+var employee = {
+  name: "John Smith",
+  job: "Programmer",
+  age: 31
+
+  lastName : function(){
+    var S = this.name.split(" ");
+    console.log(S[1]);
+  }
+}
+
+// Add a method called lastName that prints
+// out the employee's last name to the console.
+
+// You will need to figure out how to split a string to an array.
+// Hint: http://www.w3schools.com/jsref/jsref_split.asp
